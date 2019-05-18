1cb2cfc6f70be89d16a42bad484290b80f60b66c
tomastello <44135714+tomastello@users.noreply.github.com>
2019-05-18 13:54:18 +0200

    Data exploration and script creation

---
+++ scripts/load_libraries.R
@@ -0,0 +1,15 @@
+load_reqs <- function(reqs) {
+  for(pkg in reqs) {
+    if (!(pkg %in% installed.packages())) { install.packages(pkg)}
+    
+    library(pkg, character.only = T)
+  }
+}
+
+pkgs <- c("data.table", "lubridate", "ggplot2", "ggrepel", "ggiraph", "plotly", "caret", "bit64")
+
+load_reqs(pkgs)
+
+library(ggplot2)
+options(scipen = 999)
+theme_set(theme_minimal(base_size = 16))
